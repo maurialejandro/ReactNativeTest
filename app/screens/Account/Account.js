c0163e5eec2f7e2d146f5cc7e2265224a8ebe767
mauri.alejandro2165@gmail.com <mauri.alejandro2165@gmail.com>
2021-08-30 21:06:15 -0400

    Creacion de register y login

--- app/screens/Account/Account.js
+++ app/screens/Account/Account.js
@@ -1,22 +1,43 @@
 import React, { useState, useEffect } from "react";
-import { View, Text } from "react-native";
+import { View, Text, AsyncStorage } from "react-native";
 
 import UserGuest from "./UserGuest";
 import UaserLogged from "./UserLogged";
 
 export default function Account(){
-
-    const login = useState(null);
+    
+    _storeData = async () => {
+        try {
+            await AsyncStorage.setItem(
+                '@MySuperStore:123456',
+                'I like to save it.'
+            );
+        } catch (error) {
+          // Error saving data
+        }
+    };
+    _retrieveData = async () => {
+  try {
+    const value = await AsyncStorage.getAllKeys();
+ 
+    if (value !== null) {
+      // We have data!!
+      console.log(value);
+    }
+  } catch (error) {
+    // Error retrieving data
+  }
+};const login = useState(null);
     const setLogin = useState(null); 
     const [data, setData] = useState([]);
 
     // Obtener variables del login 
-    
     useEffect(() => {
         // conectar con api User esta log enviar true si no enviar false 
         // configuracion 
         // login si no esta registrado crear componente de registro 
-
+        _storeData();
+        _retrieveData();
     })
 
     return (
